9334bf928c604fa5c2dc7b316e5c35ae0d5626d3
zichen <zichen2019@163.com>
2021-03-07 14:09:03 +0800

    fix: 修改gitignore

---
+++ vue-book-keeping/src/views/book-list/index.vue
@@ -0,0 +1,206 @@
+<template>
+  <div class="book-list">
+    <header class="book-list-detail-header">
+      <!-- header -->
+      <van-nav-bar title="Book记账" />
+      <!-- sub header -->
+      <div class="header-statistics">
+        <van-row gutter="24">
+          <van-col
+            v-for="(item, index) in topAnalyFields"
+            :span="item.span"
+            :key="index"
+          >
+          <block-item
+            :template="item.template"
+            :propsData="item.templateData"
+          >
+            <div slot="right" class="vertacal-divider" v-if="index === 0"></div>
+          </block-item>
+          </van-col>
+        </van-row>
+      </div>
+    </header>
+
+    <div class="book-content">
+      <List :listData="listData" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { NavBar, Col, Row } from 'vant';
+import BlockItem from '@/components/BlockItem';
+import List from '@/components/List';
+export default {
+  name: 'book-list',
+  components: {
+    [NavBar.name]: NavBar,
+    [Col.name]: Col,
+    [Row.name]: Row,
+    BlockItem,
+    List
+  },
+  data() {
+    return {
+      topAnalyFields: [{ 
+        span: 6, 
+        template: {
+          type: {
+            type: 'select',
+            direction: 'right-bottom',
+          },
+          subField: {
+            name: '年',
+            dataIndex: 'Year',
+            dataDirect: 'left',
+          },
+          field: {
+            name: '月',
+            dataIndex: 'Month',
+            dataDirect: 'left',
+          }
+        },
+        templateData: {
+          'Year': '2018',
+          'Month': '6',
+        }
+      },{ 
+        span: 9, 
+        template: {
+          subField: {
+            name: '收入',
+          },
+          field: {
+            dataIndex: 'income',
+            isFormat: true,
+            precision: 2,
+            isallowTh: false
+          },
+          textAlign: 'left'
+        },
+          templateData: {
+          'income': '0.00',
+        }
+      },{
+        span: 9,
+        template: {
+          subField: {
+            name: '支出',
+          },
+          field: {
+            dataIndex: 'payment',
+            isFormat: true,
+            precision: 2,
+            isallowTh: false
+          },
+          textAlign: 'left'
+        },
+        templateData: {
+          'payment': '5598.97',
+        }
+      }],
+      listData: [
+        {
+          list: [{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          }
+          ]
+        }, {
+          list: [{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          },{
+            iconUrl: require('@/assets/logo.png'),
+            description: '买东西， 买东西买东西买东西买东西买东西',
+            extra: '-1.5',
+          }
+          ]
+        }
+      ]
+    }
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.book-list {
+  display: flex;
+  flex-direction: row;
+  .book-list-detail-header {
+    flex: 104px;
+    .van-nav-bar {
+      background: #fad958;
+      font-family: PingFangSC-Regular, sans-serif;
+      &::after {
+        display: none;
+      }
+    }
+    .header-statistics {
+      padding-top: 6px;
+      background: #fad958;
+      .vertacal-divider {
+        position: absolute;
+        top: 50%;
+        right: -8px;
+        height: 39px;
+        width: 1px;
+        margin: 5px;
+        background: #000;
+        transform: translateY(-45%) scale(0.5);
+      }
+    }
+  }
+  .book-content {
+    flex: 1;
+    overflow-x: hidden;
+    overflow-y: scroll;
+  }
+}
+</style>
---
+++ vue-book-keeping/src/views/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <router-view />
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="home-o">标签</van-tabbar-item>
+      <van-tabbar-item name="search" icon="search">标签</van-tabbar-item>
+      <van-tabbar-item name="friends" icon="friends-o">标签</van-tabbar-item>
+      <van-tabbar-item name="setting" icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem} from 'vant';
+export default {
+  name: 'Index',
+  components: {
+    [Tabbar.name]: Tabbar,
+    [TabbarItem.name]: TabbarItem,
+  },
+  data() {
+    return {
+      active: 0
+    }
+  },
+  methods: {}
+}
+</script>
